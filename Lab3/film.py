55928cde5e5fe2f21e62357de054b37938e7fe9c
Putienko Serhiy <50183250+CrispBS@users.noreply.github.com>
2020-11-26 23:21:53 +0200

    Add lab3

---
+++ Lab3/film.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, String, Integer, Date
+
+from base import Base
+
+
+class Film(Base):
+    __tablename__ = 'Films'
+
+    id = Column(Integer, primary_key=True)
+    f_name = Column(String(20))
+    release_year = Column(Integer)
+    f_genre = Column(String(10))
+    f_duration = Column(Integer)
+
+    def __repr__(self):
+        return "<Films('%s', '%s', '%s', '%s')>" % (self.f_name, self.release_year, self.f_genre, self.f_duration)
+
